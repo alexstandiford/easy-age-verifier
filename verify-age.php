af2008a246ecb9316d9ce154751095575d9feef7
Alex Standiford <tstandiford@gmail.com>
2016-06-18 07:02:42 -0400

    Replaced JS and CSS script enqueue method with the simpler WordPress method. resolves issue #2

--- verify-age.php
+++ verify-age.php
@@ -6,28 +6,17 @@ Version:     1.0
 Author:      Alex Standiford
 Author URI:  http://www.alexstandiford.com
 */
-$taseav_script = plugin_dir_url(__FILE__).'verify-age.js';
-$taseav_css = plugin_dir_url(__FILE__).'verify-age.css';
 
-function taseav_add_script(){
-  global $taseav_script;
-  echo '<!---Age Verification Script--->';
-  echo '<script src="'.$taseav_script.'"></script>';
+//Enqueues scripts and styles
+function taseav_init(){
+  //Calls jQuery beforehand as verify-age depends on it
+  wp_enqueue_script('jquery');
+  //Age Verification Script
+  wp_enqueue_script('verify-age.js',plugin_dir_url(__FILE__).'verify-age.js');
+  
+  //Age Verification Style
+  wp_enqueue_style('verify-age.css',plugin_dir_url(__FILE__).'verify-age.css');
 }
-
-function taseav_add_css(){
-  global $taseav_css;
-  echo '<!---Age Verification CSS--->';
-  echo '<link rel="stylesheet" type="text/css" href="'.$taseav_css.'">';
-}
-
-if(function_exists('tas_add_tag')){
-  tas_add_tag($taseav_script,'script','Age Verification Script','footer');
-  tas_add_tag($taseav_css,'stylesheet','Age Verification Stylesheet');
-}
-else{
-  add_action('wp_footer','taseav_add_script');
-  add_action('wp_head','taseav_add_css');
-}
+add_action('wp_enqueue_scripts','taseav_init');
 
 ?>
