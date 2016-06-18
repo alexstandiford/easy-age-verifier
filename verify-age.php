993e1a6ccff7dfb05352d2cd7e310f3ed6629be1
Alex Standiford <tstandiford@gmail.com>
2016-06-18 08:20:36 -0400

    Added a class that calculates age based on the cookie value (if it exists)

--- verify-age.php
+++ verify-age.php
@@ -7,6 +7,47 @@ Author:      Alex Standiford
 Author URI:  http://www.alexstandiford.com
 */
 
+class taseav{
+  
+  public function __construct(){
+    $this->dob = $_COOKIE['dob'];
+    $this->minAge = '21';
+  }
+  
+  public function isOfAge(){
+    if($this->age() >= $this->minAge && $this->age() !== false){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  
+  public function age(){
+    if(isset($this->dob)){
+    //explode the date to get month, day and year
+    $birthDate = explode("-", $this->dob);
+    //get age from date or birthdate
+    $age = (date("Ymd", date("U", mktime(0, 0, 0, $birthDate[2], $birthDate[0], $birthDate[1]))) > date("Ymd")
+         ? ((date("Y") - $birthDate[0]) - 1)
+         : (date("Y") - $birthDate[0]));
+    return $age;
+    }
+    else{
+      return false;
+    }
+  }
+  
+  public function get(){
+    $result = [];
+    foreach($this as $var => $value){
+      $result = array_merge($result,[$var => $value]);
+    }
+    return $result;
+  }
+  
+}
+
 //Enqueues scripts and styles
 function taseav_init(){
   //Calls jQuery beforehand as verify-age depends on it
