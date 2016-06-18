dee50d6550a069cf1bcde67dc6c4291a62806a63
Alex Standiford <tstandiford@gmail.com>
2016-06-18 08:21:52 -0400

    Added if statement to confirm age before scripts are enqueued

--- verify-age.php
+++ verify-age.php
@@ -50,13 +50,25 @@ class taseav{
 
 //Enqueues scripts and styles
 function taseav_init(){
-  //Calls jQuery beforehand as verify-age depends on it
-  wp_enqueue_script('jquery');
-  //Age Verification Script
-  wp_enqueue_script('verify-age.js',plugin_dir_url(__FILE__).'verify-age.js');
-  
-  //Age Verification Style
-  wp_enqueue_style('verify-age.css',plugin_dir_url(__FILE__).'verify-age.css');
+  //Calls the data to pass to the JS file
+  $pass_data = new taseav();
+  //Checks to see if the date of birth is above the desired age
+  if($pass_data->isOfAge() == false){
+    //Calls jQuery beforehand as verify-age depends on it
+    wp_enqueue_script('jquery');
+
+    //Registers the Age Verification Script
+    wp_register_script('verify-age.js',plugin_dir_url(__FILE__).'verify-age.js');
+
+    //Adds PHP Variables to the script as an object
+    wp_localize_script('verify-age.js','taseavData',$pass_data->get());
+
+    //Calls Age Verification Script
+    wp_enqueue_script('verify-age.js');
+
+    //Age Verification Style
+    wp_enqueue_style('verify-age.css',plugin_dir_url(__FILE__).'verify-age.css');
+  }
 }
 add_action('wp_enqueue_scripts','taseav_init');
 
