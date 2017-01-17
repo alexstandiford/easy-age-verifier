9fca07f2ef946f32237b16b9ae699922bc1a0f1f
Alex Standiford <tstandiford@gmail.com>
2017-01-17 06:09:59 -0500

    Created the verification class. This class houses all of the functions that check if the user is of age or not.

---
+++ lib/app/verification.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Verification class for Easy Age Verifier
+ * Everything related to the verification process of the visitor
+ * @author: Alex Standiford
+ * @date  : 1/15/2017
+ */
+
+namespace eav\app;
+
+use eav\config\option;
+
+class verification{
+
+  public $isOfAge = null;
+
+  public function __construct($dob = null){
+    $this->minimum_age = option::getWithFilter('minimum_age', 21, 'eav_default_age');
+    $this->visitor_age = age::getFromDob($dob);
+  }
+
+  /**
+   * Checks if the visitor is of-age. Stores the result into a value, so it doesn't need to be re-ran
+   * @return bool
+   */
+  public function isOfAge(){
+    if(isset($this->isOfAge)){
+      return $this->isOfAge;
+    }
+
+    $checks = array(
+      $this->visitor_age >= $this->minimum_age,
+      $this->visitor_age != false,
+      $this->visitor_age != 'underAge',
+    );
+
+    if(in_array(true, $checks)){
+      $this->isOfAge = true;
+
+      return true;
+    }
+    else{
+      $this->isOfAge = false;
+
+      return false;
+    }
+  }
+
+  /**
+   * Allows developers to add custom logic for the modal popup
+   * @return array|mixed
+   */
+  public function customIsTrue(){
+    $checks = array('result' => true);
+    $checks = apply_filters('eav_custom_modal_logic', $checks);
+    if(is_array($checks)){
+      foreach($checks as $check){
+        if($check == false){
+          $checks['result'] = false;
+          break;
+        }
+      }
+    }
+
+    return $checks;
+  }
+
+  /**
+   * Checks if verification has failed or passed
+   * @return bool
+   */
+  public function failed(){
+    $checks = array(
+      $this->isOfAge() == false,
+      !is_user_logged_in(),
+    );
+
+    if(in_array(true,$checks)){
+      $failed = true;
+    }
+    else{
+      if($this->customIsTrue()['result']){
+        $failed = false;
+      }
+      else{
+        $failed = true;
+      }
+    }
+    return $failed;
+  }
+}
