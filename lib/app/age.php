0c76544ac9907466377f735d68b49759fa7511e5
Alex Standiford <tstandiford@gmail.com>
2017-01-17 08:18:43 -0500

    Refactored the age class to be more read-able

--- lib/app/age.php
+++ lib/app/age.php
@@ -11,32 +11,81 @@ namespace eav\app;
 class age{
 
   /**
+   * Parses the date of birth value into the age
+   *
+   * @param $dob
+   *
+   * @return false|string
+   */
+  private function parseAge($dob){
+    //explode the date to parseAge month, day and year
+    $birthDate = explode("-", $dob);
+    //parseAge age from date or birthdate
+    $age = (date("Ymd", date("U", mktime(0, 0, 0, $birthDate[2], $birthDate[0], $birthDate[1]))) > date("Ymd")
+      ? ((date("Y") - $birthDate[0]) - 1)
+      : (date("Y") - $birthDate[0]));
+
+    return $age;
+  }
+
+  /**
+   * Checks if the confirmation age type is a date of birth, or if it's just a "yes/no" verification
+   *
+   * @param $dob
+   *
+   * @return bool
+   */
+  private function isConfirmAge($dob){
+    if(($dob == 'overAge' || $dob == 'underAge')){
+      $confirm_age = true;
+    }
+    else{
+      $confirm_age = false;
+    }
+
+    return $confirm_age;
+  }
+
+  /**
+   * Gets the evaluated age based on confirmation age type
+   *
+   * @param $dob
+   *
+   * @return false|string
+   */
+  private function getAgeFrom($dob){
+    if(self::isConfirmAge($dob)){
+      $age = $dob;
+    }
+    else{
+      $age = self::parseAge($dob);
+    }
+
+    return $age;
+  }
+
+  /**
    * Parses the age from the date of birth
+   *
    * @param null $dob
    *
    * @return bool|false|null|string
    */
-  //TODO: Make getFromDob more readable by breaking the function down into more simple parts
   public static function getFromDob($dob = null){
+
+    //Sets default value when a dob isn't specified
     if(!isset($dob)){
       $dob = $_COOKIE['taseavdob'];
     }
+
+    //If the cookie value is set, get the age from the date of birth. Otherwise, return false.
     if(isset($dob)){
-      if(($dob == 'overAge' || $dob == 'underAge')){
-        $age = $dob;
-      }
-      else{
-        //explode the date to get month, day and year
-        $birthDate = explode("-", $dob);
-        //get age from date or birthdate
-        $age = (date("Ymd", date("U", mktime(0, 0, 0, $birthDate[2], $birthDate[0], $birthDate[1]))) > date("Ymd")
-          ? ((date("Y") - $birthDate[0]) - 1)
-          : (date("Y") - $birthDate[0]));
-      }
+      $age = self::getAgeFrom($dob);
     }
     else{
       $age = false;
     }
+
     return $age;
   }
 }
