adbf2eb66d05e868ae2eaceeae27b400fc65d55b
Alex Standiford <tstandiford@gmail.com>
2017-01-17 06:15:43 -0500

    Built the options class. This singleton class grabs all of the options from the database, and saves them as a static value in the class. This ensures that there is only one database call to collect all of the data needed to build the verifier. It also has some helper functions that make it easier to add a filter as a default value if the option value is not set.

---
+++ lib/config/option.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Options handler for Easy Age Verifier
+ * @author: Alex Standiford
+ * @date  : 1/15/2017
+ */
+
+namespace eav\config;
+
+
+class option{
+
+  private static $options = null;
+
+  /**
+   * Gets the options value for Easy Age Verifier
+   *
+   * @param $value
+   *
+   * @return mixed
+   */
+  public static function get($value, $default = null){
+
+    if(!isset(self::$options)){
+      self::$options = get_option('eav_options');
+    }
+    $db_value = self::$options['eav_'.$value];
+    if(isset($default) && !isset($db_value)){
+      $option = $default;
+    }
+    else{
+      $option = $db_value;
+    }
+
+    return $option;
+  }
+
+  /**
+   * Gets an option with a built-in default value as a filter
+   *
+   * @param      $value
+   * @param      $default
+   * @param null $filter
+   *
+   * @return mixed
+   */
+  public static function getWithFilter($value, $default, $filter = null){
+    if(self::get($value) !== ''){
+      $result = self::get($value);
+    }
+    else{
+      if(isset($filter)){
+        $result = apply_filters($filter, $default);
+      }
+      else{
+        $result = apply_filters('eav_default_'.$value.'_value', $default);
+      }
+    }
+
+    return $result;
+  }
+
+}
