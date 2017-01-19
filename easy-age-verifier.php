7dc5957c269fbdac7213904b36c42861ed38190e
Alex Standiford <tstandiford@gmail.com>
2017-01-19 16:26:47 -0500

    Implemented support for sidebar menu pages

--- easy-age-verifier.php
+++ easy-age-verifier.php
@@ -15,6 +15,7 @@ namespace eav;
 use eav\app\verifier;
 use eav\config\customizer;
 use eav\config\upgrade;
+use eav\config\menu;
 
 if(!defined('ABSPATH')) exit;
 
@@ -29,7 +30,6 @@ if(!class_exists('eav')){
 
     /**
      * Fires up the plugin.
-     *
      * @return self
      */
     public static function getInstance(){
@@ -44,7 +44,6 @@ if(!class_exists('eav')){
 
     /**
      * Defines the constants related to Easy Age Verifier
-     *
      * @return void
      */
     private function _defineConstants(){
@@ -53,13 +52,12 @@ if(!class_exists('eav')){
       define('EAV_ASSETS_URL', EAV_URL.'lib/assets/');
       define('EAV_ASSETS_PATH', EAV_PATH.'lib/assets/');
       define('EAV_TEMPLATES_PATH', EAV_ASSETS_URL.'templates/');
-      define('EAV_TEXT_DOMAIN','easyageverifier');
-      define('EAV_PREFIX','eav');
+      define('EAV_TEXT_DOMAIN', 'easyageverifier');
+      define('EAV_PREFIX', 'eav');
     }
 
     /**
      * Grabs the files to include, and requires them
-     *
      * @return void
      */
     private function _includeFiles(){
@@ -69,11 +67,15 @@ if(!class_exists('eav')){
         'config/customizer.php',
         'config/option.php',
         'config/upgrade.php',
+        'config/menu.php',
 
         //App classes
         'app/age.php',
         'app/verification.php',
         'app/verifier.php',
+
+        //Extra classes
+        'extras/wpApi.php',
 
       );
 
@@ -89,7 +91,6 @@ eav::getInstance();
 
 /**
  * Initializes the verifier form
- *
  * @return void
  */
 function init(){
@@ -97,26 +98,53 @@ function init(){
     verifier::doFormActions();
   }
 }
+
 add_action('get_header', __NAMESPACE__.'\\init');
 
 /**
  * Initializes the customizer on the admin
- *
  * @return void
- *
  */
-function admin_init(){
-    customizer::register();
+function customize_init(){
+  customizer::register();
 }
-add_action('customize_register',__NAMESPACE__.'\\admin_init');
+
+add_action('customize_register', __NAMESPACE__.'\\customize_init');
+
+/**
+ * Initializes the menu item on the admin
+ * @return void
+ */
+function menu_init(){
+  menu::register();
+}
+
+add_action('admin_menu', __NAMESPACE__.'\\menu_init');
 
 
 /**
  * Upgrades the legacy database to the new database format on plugin activation
- *
  * @return void
  */
 function upgrade_legacy_data(){
   upgrade::legacyDatabase();
 }
-register_activation_hook(__FILE__,__NAMESPACE__.'\\upgrade_legacy_data');
+
+register_activation_hook(__FILE__, __NAMESPACE__.'\\upgrade_legacy_data');
+
+function eav_admin_styles_init(){
+  $styles = array(
+    'settings.css' => EAV_ASSETS_URL.'css/settings.css',
+  );
+  $styles = apply_filters(EAV_PREFIX.'_admin_styles', $styles);
+  foreach($styles as $style => $path){
+    wp_enqueue_style($style, $path);
+  }
+  $scripts = array();
+  $scripts = apply_filters(EAV_PREFIX.'_admin_scripts', $scripts);
+  foreach($scripts as $script => $parameters){
+    wp_enqueue_script($script, $parameters['src'], $parameters['dependencies'], $parameters['version'], $parameters['in_footer']);
+  }
+}
+
+add_action('admin_enqueue_scripts', __NAMESPACE__.'\\eav_admin_styles_init');
