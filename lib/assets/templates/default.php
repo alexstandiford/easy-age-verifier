62891bebee1254586a3683a683cc19011068499a
Alex Standiford <tstandiford@gmail.com>
2017-02-19 11:07:43 -0500

    Updated the default template to instead grab form items from the verifier object, instead of passed POST data

--- lib/assets/templates/default.php
+++ lib/assets/templates/default.php
@@ -4,39 +4,37 @@
  * @author: Alex Standiford
  * @date  : 1/15/2017
  */
-
-$verifier = $_POST;
 ?>
-<div id='taseav-age-verify' class='<?php echo $verifier['wrapperClass']; ?>'>
-  <?php echo $verifier['beforeForm']; ?>
-  <form class='<?php echo $verifier['formClass']; ?>'>
-    <h2><?php echo $verifier['formTitle'] ?></h2>
+<div id='taseav-age-verify' class='<?php echo $this->wrapperClass; ?>'>
+  <?php echo $this->beforeForm; ?>
+  <form class='<?php echo $this->formClass; ?>'>
+    <h2><?php echo $this->formTitle ?></h2>
     <?php
     //If the settings call to enter the age, do this
-    if($verifier['formType'] == 'eav_enter_age'): ?>
-      <?php echo $verifier['beforeMonth']; ?>
-      <div class='<?php echo $verifier['monthClass']; ?>'>
+    if($this->formType == 'eav_enter_age'): ?>
+      <?php echo $this->beforeMonth; ?>
+      <div class='<?php echo $this->monthClass; ?>'>
         <label for="month">Month</label>
         <input tabindex="1" title="month" name='month' type='number' min='1' max='12' required>
       </div>
-      <?php echo $verifier['beforeDay']; ?>
-      <div class='<?php echo $verifier['dayClass']; ?>'>
+      <?php echo $this->beforeDay; ?>
+      <div class='<?php echo $this->dayClass; ?>'>
         <label for="day">Day</label>
         <input tabindex="2" title="day" name='day' type='number' min='1' max='31' required>
       </div>
-      <?php echo $verifier['beforeYear']; ?>
-      <div class='<?php echo $verifier['yearClass']; ?>'>
+      <?php echo $this->beforeYear; ?>
+      <div class='<?php echo $this->yearClass; ?>'>
         <label for="year">Year</label>
-        <input tabindex="3" title="year" name='year' type='number' min='<?php echo $verifier['minYear']; ?>'
+        <input tabindex="3" title="year" name='year' type='number' min='<?php echo $this->minYear; ?>'
                max='<?php echo date("Y"); ?>' required>
       </div>
-      <?php echo $verifier['beforeButton']; ?>
-      <input tabindex="4" type='submit' value='<?php echo $verifier['buttonValue']; ?>'>
+      <?php echo $this->beforeButton; ?>
+      <input tabindex="4" type='submit' value='<?php echo $this->buttonValue; ?>'>
     <?php endif; ?>
-    <?php if($verifier['formType'] == 'eav_confirm_age'): ?>
-      <input name='overAge' type='submit' value='<?php echo $verifier['overAge']; ?>'>
-      <input name='underAge' type='submit' value='<?php echo $verifier['underAge']; ?>'>
+    <?php if($this->formType == 'eav_confirm_age'): ?>
+      <input name='overAge' type='submit' value='<?php echo $this->overAge; ?>'>
+      <input name='underAge' type='submit' value='<?php echo $this->underAge; ?>'>
     <?php endif; ?>
   </form>
-  <?php $verifier['afterForm']; ?>
+  <?php $this->afterForm; ?>
 </div>
