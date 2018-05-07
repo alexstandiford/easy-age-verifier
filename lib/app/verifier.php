3c0f4a5e4b6635451a7194d6a1a373f4275a0659
Alex Standiford <a@alexstandiford.com>
2018-05-07 15:03:34 -0400

    Bumped Version

--- lib/app/verifier.php
+++ lib/app/verifier.php
@@ -150,13 +150,13 @@ class verifier{
     //Calls jQuery beforehand as verify-age depends on it
     wp_enqueue_script('jquery');
     //Registers the Age Verification Script
-    wp_register_script('verify-age.js', EAV_ASSETS_URL.'js/verifier.js', array());
+    wp_register_script('verify-age.js', EAV_ASSETS_URL.'js/verifier.js', array(),'1.50');
     //Adds PHP Variables to the script as an object
     wp_localize_script('verify-age.js', 'eav', $this->passData());
     //Calls Age Verification Script
     wp_enqueue_script('verify-age.js', array());
     //Age Verification Style
-    wp_enqueue_style('verify-age.css', EAV_ASSETS_URL.'/css/verifier.css', array(), '1.30');
+    wp_enqueue_style('verify-age.css', EAV_ASSETS_URL.'/css/verifier.css', array(), '1.50');
   }
 
   /**
