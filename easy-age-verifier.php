8def38cc6a5e76e9b3e0862b69063a13d56c8b66
Alex Standiford <a@alexstandiford.com>
2018-05-09 11:13:30 -0400

    Added a new constant, EAV_VERSION
    This is going to be used throughout all wp_enqueue_script references, and is automatically generated based on what the plugin data's version is.
    
    I kept forgetting to update my script version tag, so I decided to automate it instead.

--- easy-age-verifier.php
+++ easy-age-verifier.php
@@ -61,6 +61,10 @@ if(!class_exists('eav')){
       define('EAV_TEMPLATES_PATH', EAV_ASSETS_PATH.'templates/');
       define('EAV_TEXT_DOMAIN', 'easyageverifier');
       define('EAV_PREFIX', 'eav');
+
+      $version = get_plugin_data(__FILE__,false,false);
+      $version = $version['Version'];
+      define('EAV_VERSION', $version);
     }
 
     /**
